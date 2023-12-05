fe31f4665f5ac1091075df2300579f11e2862adb
Adrian Silva <adriansilva296@gmail.com>
2023-12-05 20:27:03 -0300

    initial commit
    casos de teste de uso para verificação e validação P1

---
+++ cypress/e2e/cenario1.spec.cy.js
@@ -0,0 +1,27 @@
+//cenário 1
+describe('Fluxo de login', () => {
+  it('passes', () => {
+    cy.visit('https://saucedemo.com')
+    cy.get('#user-name').type('problem_user')
+    cy.get('#password').type('secret_sauce')
+
+    cy.get('form').submit();
+
+    cy.url().should('eq', 'https://www.saucedemo.com/inventory.html');
+
+    cy.get('#react-burger-menu-btn').should('exist').click();
+    cy.get('#shopping_cart_container').should('exist').click();
+    cy.get('#continue-shopping').should('exist').click();
+    cy.get('#item_4_title_link').should('exist').click();
+    cy.get('#back-to-products').should('exist').click();
+    cy.get('#add-to-cart-sauce-labs-backpack').should('exist').click();
+  })
+})
+
+/*
+  !Cenário 1: Login e Navegação
+  ?1. Você deve testar o fluxo de login no Sauce Demo.
+  ?2. Verificar se o usuário é redirecionado corretamente para a página inicial após o login.
+  ?3. Verificar se o menu de navegação está funcionando corretamente, permitindo a transição entre diferentes páginas.
+  ?4. Verificar se os elementos de navegação (links, botões etc.) estão corretamente posicionados e funcionais em todas as páginas
+*/
